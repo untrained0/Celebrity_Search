cd23846eedf401207fe2bfadaea914d2d94a49c9
Soham <sohamkelaskar@gmail.com>
2024-05-12 01:17:00 +0530

    Memory Stored

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 import streamlit as st
 
@@ -19,23 +20,28 @@ first_input_prompt = PromptTemplate(
     template= "Tell me about celebrity {name}"
 )
 
+# Memory
+person_memory = ConversationBufferMemory(imput_key = "name", memory_key='name_history')
+dob_memory = ConversationBufferMemory(imput_key = "person", memory_key='dob_history')
+events_memory = ConversationBufferMemory(imput_key = "dob", memory_key='events_history')
+
 # OPENAI LLMS
 llm = OpenAI(temperature=0.8)
-chain1 = LLMChain(llm = llm, prompt=first_input_prompt, verbose=True, output_key='person')
+chain1 = LLMChain(llm = llm, prompt=first_input_prompt, verbose=True, output_key='person', memory=person_memory)
 
 second_input_prompt = PromptTemplate(
     input_variables=["person"],
     template= "When was {person} born"
 )
 
-chain2 = LLMChain(llm = llm, prompt=second_input_prompt, verbose=True, output_key='dob')
+chain2 = LLMChain(llm = llm, prompt=second_input_prompt, verbose=True, output_key='dob', memory=dob_memory)
 
 third_input_prompt = PromptTemplate(
     input_variables=["dob"],
     template= "What 5 major events occurred on {dob}"
 )
 
-chain3 = LLMChain(llm = llm, prompt=third_input_prompt, verbose=True, output_key='events')
+chain3 = LLMChain(llm = llm, prompt=third_input_prompt, verbose=True, output_key='events', memory=events_memory)
 
 parent_chain = SequentialChain(chains=[chain1, chain2, chain3], input_variables=['name'], output_variables=['person', 'dob', 'events'], verbose=True)
 
@@ -53,3 +59,9 @@ SequentialChain.run = run
 if input_text:
     result = parent_chain.run({'name': input_text})
     st.write(result)
+
+    with st.expander('Person Name'):
+        st.info(person_memory.buffer)
+
+    with st.expander('Events Occured'):
+        st.info(events_memory.buffer)
