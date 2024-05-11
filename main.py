29c7e2e40acdbf222cb08ed2be338fdbcdf4f40a
Soham <sohamkelaskar@gmail.com>
2024-05-12 00:43:04 +0530

    Sequential Chain feature added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import openai_key
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
 
 import streamlit as st
 
@@ -31,7 +32,18 @@ first_input_prompt = PromptTemplate(
 
 # OPENAI LLMS
 llm = OpenAI(temperature=0.8)
-chain = LLMChain(llm = llm, prompt=first_input_prompt, verbose=True)
+chain1 = LLMChain(llm = llm, prompt=first_input_prompt, verbose=True, output_key='person')
+
+#  Prompt Template
+second_input_prompt = PromptTemplate(
+    input_variables=["person"],
+    template= "When was {person} born"
+)
+
+chain2 = LLMChain(llm = llm, prompt=second_input_prompt, verbose=True, output_key='dob')
+
+parent_chain = SimpleSequentialChain(chains=[chain1, chain2], verbose=True)
+
 
 if input_text:
-    st.write(chain.run(input_text))
+    st.write(parent_chain.run(input_text))
